112ee78d36ba313f0589082a7af3c4809f9554af
Gustavo Rostirolla <gustavorostirollax@gmail.com>
2023-07-13 21:27:44 -0300

    4 commit

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,45 +1,41 @@
-import "./signin.css";
 import { useState } from "react";
+import "./signin.css";
 
 import logoclinica from "../../assets/logoclinica.png";
+import { Link } from "react-router-dom";
 
-function SignIn() {
+export default function SignIn() {
   const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
-    <>
-      <div className="container-center">
-        <div className="login">
-          <div className="login-area">
-            <img src={logoclinica} alt="Logo do sistema" />
-          </div>
-          <form>
-            <h1>Entrar</h1>
-            <input
-              type="text"
-              placeholder="email"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-            <input
-              type="password"
-              placeholder="*******"
-              value={pass}
-              onChange={(e) => {
-                setPass(e.target.value);
-              }}
-            />
-            <input type="submit" value="Acessar" />
-          </form>
-
-          <Link to="/register">Criar uma conta</Link>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logoclinica} alt="Logo do sistema de chamados" />
         </div>
+
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
       </div>
-    </>
+    </div>
   );
 }
-
-export default SignIn;
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,50 @@
-function SignUp() {
+import { useState } from "react";
+import "./signup.css";
+import { Link } from "react-router-dom";
+
+import logoclinica from "../../assets/logoclinica.png";
+
+export default function SignUp() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <>
-      <div>
-        <span>Cadastro</span>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logoclinica} alt="Logo do sistema de chamados" />
+        </div>
+
+        <form className="form">
+          <h1>Cadastrar nova conta</h1>
+          <input
+            type="text"
+            placeholder="nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit">Cadastrar</button>
+
+          <div className="toLogin">
+            <Link to="/">Fazer login</Link>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
-
-export default SignUp;
